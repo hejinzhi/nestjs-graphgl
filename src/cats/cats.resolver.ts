5dd76cfa1a3592d046eadad132b6f97e697e8bc3
hejinzhi <504151911@qq.com>
2021-12-30 10:46:38 +0800

    feat: init

---
+++ src/cats/cats.resolver.ts
@@ -0,0 +1,48 @@
+import { ParseIntPipe, UseGuards } from '@nestjs/common';
+import { Args, Mutation, Query, Resolver, Subscription } from '@nestjs/graphql';
+import { PubSub } from 'graphql-subscriptions';
+import { Cat } from '../graphql.schema';
+import { CatsGuard } from './cats.guard';
+import { CatsService } from './cats.service';
+import { CreateCatDto } from './dto/create-cat.dto';
+
+const pubSub = new PubSub();
+
+@Resolver('Cat')
+export class CatsResolver {
+  constructor(private readonly catsService: CatsService) {}
+
+  @Query('cats')
+  @UseGuards(CatsGuard)
+  async getCats() {
+    return this.catsService.findAll();
+  }
+
+  @Query('cat')
+  async findOneById(
+    @Args('id', ParseIntPipe)
+    id: number,
+  ): Promise<Cat> {
+    console.log(id)
+    return this.catsService.findOneById(id);
+  }
+
+  @Mutation('createCat')
+  async create(@Args('createCatInput') args: CreateCatDto): Promise<Cat> {
+    const createdCat = await this.catsService.create(args);
+    pubSub.publish('catCreated', { catCreated: createdCat });
+    return createdCat;
+  }
+
+  @Mutation('deleteCat')
+  async delete(@Args('id') id: number): Promise<Cat> {
+    const deletedCat = await this.catsService.delete(id); 
+    // pubSub.publish('catCreated', { catCreated: createdCat });
+    return deletedCat;
+  }
+
+  @Subscription('catCreated')
+  catCreated() {
+    return pubSub.asyncIterator('catCreated');
+  }
+}
---
+++ src/cats/cats.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@nestjs/common';
+import { Cat } from '../graphql.schema';
+
+@Injectable()
+export class CatsService {
+  private readonly cats: Array<Cat & { ownerId?: number }> = [
+    { id: 1, name: 'Cat', age: 5, ownerId: 1 },
+  ];
+
+  create(cat: Cat): Cat {
+    cat.id = this.cats.length + 1;
+    this.cats.push(cat);
+    return cat;
+  }
+
+  findAll(): Cat[] {
+    return this.cats;
+  }
+
+  findOneById(id: number): Cat {
+    return this.cats.find(cat => cat.id === id);
+  }
+
+  delete(id:number) {
+    const index = this.cats.findIndex((v)=>v.id === id)
+    const cat = this.cats[index]
+    this.cats.splice(index,1)
+    return cat
+  }
+}
